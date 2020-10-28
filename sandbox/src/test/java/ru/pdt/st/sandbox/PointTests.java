6a5854d5ebeedc704fd394cd18de98593e5e0ca6
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-10-28 23:11:55 +0300

    Задание 3. Добавлены тесты для класса Point

---
+++ sandbox/src/test/java/ru/pdt/st/sandbox/PointTests.java
@@ -0,0 +1,29 @@
+package ru.pdt.st.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance() {
+    Point P1 = new Point(1, 2);
+    Point P2 = new Point(3, 4);
+    Assert.assertEquals(P2.distance(P1), Math.sqrt(8) );
+  }
+
+  @Test
+  public void testDistancePositive() {
+    Point P1 = new Point(1, 2);
+    Point P2 = new Point(3, 4);
+    Assert.assertEquals(P2.x - P1.x, 2 );
+  }
+
+  @Test
+  public void testDdistanceNegative() {
+    Point P1 = new Point(1, 2);
+    Point P2 = new Point(3, 4);
+    Assert.assertEquals(P2.x - P1.x, 20 );
+  }
+
+}
---
+++ sandbox/src/test/java/ru/pdt/st/sandbox/SquareTests.java
@@ -0,0 +1,14 @@
+package ru.pdt.st.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+
+  @Test
+  public void testArea() {
+    Square s = new Square(5);
+    s.area();
+    Assert.assertEquals(s.area(), 25.0);
+  }
+}
