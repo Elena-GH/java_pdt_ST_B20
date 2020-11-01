fcb6483f3f0aa204e6d8429851beb93170de29cc
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-01 20:35:00 +0300

    Добавлен тестовый класс ContactModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,20 @@
+package ru.pdt.st.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.pdt.st.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+  @Test
+  public void testContactModification() {
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactForm(new ContactData(
+            "New_Contact_First_Name",
+            "New_Contact_Last_Name",
+            "+7 (321) 123-45-67",
+            "new_contact_mail@gmail.com"));
+    app.getContactHelper().submitContactModification();
+    app.getContactHelper().returnHomePage();
+  }
+
+}
