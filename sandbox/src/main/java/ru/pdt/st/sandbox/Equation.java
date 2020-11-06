2f41b761ba9d1ebe28bea13d7e2e4e38322d4aeb
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-06 23:11:45 +0300

    Улучшена читабельность для с == 0

--- sandbox/src/main/java/ru/pdt/st/sandbox/Equation.java
+++ sandbox/src/main/java/ru/pdt/st/sandbox/Equation.java
@@ -27,12 +27,11 @@ public class Equation {
     } else if (b != 0) {
       n = 1;
 
+    } else if (c == 0) {
+      n = -1;
+
     } else {
-      if (c == 0) {
-        n = -1;
-      } else {
-        n = 0;
-      }
+      n = 0;
     }
 
   }
