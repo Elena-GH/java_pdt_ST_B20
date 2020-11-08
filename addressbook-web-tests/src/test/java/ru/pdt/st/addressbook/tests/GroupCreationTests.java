16759e3618332fe6c51f8046d15e7e89e9e11802
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-08 15:44:53 +0300

    В testGroupDeletion добавлена проверка на наличие группы

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
@@ -8,13 +8,10 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData(
+    app.getGroupHelper().createGroup(new GroupData(
             "Group_Name",
             null,
             null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,19 @@
 package ru.pdt.st.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.pdt.st.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData(
+              "Group_Name",
+              null,
+              null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
