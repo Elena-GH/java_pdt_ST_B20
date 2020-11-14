65376b7c4ff9facf56e811cc01353d0f93db031e
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-14 21:45:51 +0300

    GroupCreationTests переведен на ArrayList для подсчета количества групп в списке

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
@@ -4,18 +4,21 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.pdt.st.addressbook.model.GroupData;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    int befor = app.getGroupHelper().getGroupCount();
+    List<GroupData> befor = app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(new GroupData(
             "Group_Name",
             null,
             null));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, befor + 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), befor.size() + 1);
   }
 
 }
