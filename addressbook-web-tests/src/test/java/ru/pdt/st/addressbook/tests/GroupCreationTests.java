537316dd985f576a5c51583909f04054547ba33e
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-15 18:13:27 +0300

    В GroupCreationTests сравнения списков befor и after реализовано с помощью множеств HashSet

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.*;
 import ru.pdt.st.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -13,12 +14,23 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> befor = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData(
+    GroupData group = new GroupData(
             "Group_Name",
             null,
-            null));
+            null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), befor.size() + 1);
+
+    int max = 0;
+    for (GroupData g : after) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    befor.add(group);
+    Assert.assertEquals(new HashSet<Object>(befor), new HashSet<Object>(after));
   }
 
 }
