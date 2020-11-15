d4ca6f7620e8898edaa62d11ca3a0ab4ecce80fb
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-15 18:41:59 +0300

    В GroupCreationTests теперь для поиска max используется лямбда-выражение

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
@@ -1,11 +1,9 @@
 package ru.pdt.st.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.pdt.st.addressbook.model.GroupData;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -16,27 +14,14 @@ public class GroupCreationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> befor = app.getGroupHelper().getGroupList();
     GroupData group = new GroupData(
-            "Group_Name",
+            "Group_Name_N",
             null,
             null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), befor.size() + 1);
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    Comparator<? super GroupData> byId = new Comparator<GroupData>() {
-      @Override
-      public int compare(GroupData o1, GroupData o2) {
-        return Integer.compare(o1.getId(), o2.getId());
-      }
-    };
-    int max1 = after.stream().max(byId).get().getId();
-    group.setId(max1);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     befor.add(group);
     Assert.assertEquals(new HashSet<Object>(befor), new HashSet<Object>(after));
   }
