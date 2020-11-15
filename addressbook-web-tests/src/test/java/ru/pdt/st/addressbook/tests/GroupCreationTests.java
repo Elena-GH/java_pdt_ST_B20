063e18c24df840ecedf0b6fae1e39c5c44704f4b
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-15 18:26:20 +0300

    В GroupCreationTests сравнение списков реализовано с помощью Stream и Comparator

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.*;
 import ru.pdt.st.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -28,7 +29,14 @@ public class GroupCreationTests extends TestBase {
         max = g.getId();
       }
     }
-    group.setId(max);
+    Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+      @Override
+      public int compare(GroupData o1, GroupData o2) {
+        return Integer.compare(o1.getId(), o2.getId());
+      }
+    };
+    int max1 = after.stream().max(byId).get().getId();
+    group.setId(max1);
     befor.add(group);
     Assert.assertEquals(new HashSet<Object>(befor), new HashSet<Object>(after));
   }
