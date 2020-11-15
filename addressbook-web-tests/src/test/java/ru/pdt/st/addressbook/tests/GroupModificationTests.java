4c97139d0f89e00759634f596da5fbd5576de99e
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-15 17:33:33 +0300

    В GroupModificationTests сравнение списков before и after реализовано с помощью множеств HashSet

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.pdt.st.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -20,14 +21,19 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> befor = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(befor.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData(
+    GroupData group = new GroupData(befor.get(befor.size() - 1).getId(),
             "Group_Name",
             "Group_Header",
-            "New_Group_Footer"));
+            "New_Group_Footer");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), befor.size());
+
+    befor.remove(befor.size() - 1);
+    befor.add(group);
+    Assert.assertEquals(new HashSet<Object>(befor), new HashSet<Object>(after));
   }
 
 }
