92cc797f208fefc04eb1c9c795467ea8693eadee
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-16 14:07:50 +0300

    ContactDeletionTests. Реализован контроль списков до и после теста. Ошибка при нахождении lastName

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/ContactDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.pdt.st.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.pdt.st.addressbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactDeletionTests extends TestBase{
 
   @Test
   public void testContactDeletion() {
+    app.getNavigationHelper().gotoHomePage();
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData(
               "Contact_First_Name",
@@ -15,10 +19,18 @@ public class ContactDeletionTests extends TestBase{
               "contact_mail@gmail.com",
               "Group_Name"));
     }
-    app.getContactHelper().selectContact();
+    List<ContactData> befor = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(befor.size() - 1);
     app.getContactHelper().deleteSelectedGroups();
     app.getContactHelper().confirmAction();
     app.getNavigationHelper().gotoHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), befor.size() - 1);
+
+    befor.remove(befor.size() - 1);
+    // Сравнение списков групп до и после теста с помощью списков (упорядоченные коллекции)
+    // При этом сравнение выполняется средствами тестовго фреймворка testng
+    Assert.assertEquals(befor, after);
   }
 
 }
