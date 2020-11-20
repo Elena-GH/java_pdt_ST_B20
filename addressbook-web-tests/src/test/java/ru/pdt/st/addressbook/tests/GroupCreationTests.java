f50501a685e63bc0415a17778e5da155df7575da
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-20 22:43:06 +0300

    GroupCreationTests переведен на использование Hamcrest и расширение ForwardingSet

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
@@ -1,27 +1,30 @@
 package ru.pdt.st.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.pdt.st.addressbook.model.GroupData;
+import ru.pdt.st.addressbook.model.Groups;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
     app.goTo().groupPage();
-    Set<GroupData> befor = app.group().all();
+    Groups befor = app.group().all();
     GroupData group = new GroupData().withName("Group_Name");
     app.group().create(group);
-    Set<GroupData> after = app.group().all();
-    Assert.assertEquals(after.size(), befor.size() + 1);
+    Groups after = app.group().all();
+    assertThat(after.size(), equalTo(befor.size() + 1));
 
-    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-    befor.add(group);
     // Сравнение списков групп до и после теста с помощью множеств (неупорядоченные коллекции)
-    // При этом сравнение выполняется средствами тестовго фреймворка testng
-    Assert.assertEquals(befor, after);
+    // Для реализации fluent-интерфейса (вытягивания в цепочку) сравниваются копии множества after и befor
+    // Расширение методов для HashSet реализуется через интерфейс ForwardingSet библиотеки Guava +withAdded
+    // При этом сравнение выполняется средствами подключенной библиотеки Hamcrest +assertThat +equalTo
+    assertThat(after, equalTo(
+            befor.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
   }
 
 }
