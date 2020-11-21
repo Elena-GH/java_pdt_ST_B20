c6b80d344c13500f5985048e8333c2e297416b53
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-21 23:44:35 +0300

    Группы. Переименована переменная для кеша списка групп

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/appmanager/GroupHelper.java
@@ -52,7 +52,7 @@ public class GroupHelper extends HelperBase {
     initGroupCreation();
     fillGroupForm(group);
     submitGroupCreation();
-    groupCash = null;
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -61,14 +61,14 @@ public class GroupHelper extends HelperBase {
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
-    groupCash = null;
+    groupCache = null;
     returnToGroupPage();
   }
 
   public void delete(GroupData group) {
     selectGroupById(group.getId());
     deleteSelectedGroups();
-    groupCash = null;
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -80,20 +80,20 @@ public class GroupHelper extends HelperBase {
     return wd.findElements(By.name("selected[]")).size();
   }
 
-  private Groups groupCash = null;
+  private Groups groupCache = null;
 
   public Groups all() {
-    if (groupCash != null) {
-      return new Groups(groupCash);
+    if (groupCache != null) {
+      return new Groups(groupCache);
     }
-    groupCash = new Groups();
+    groupCache = new Groups();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      groupCash.add(new GroupData().withId(id).withName(name));
+      groupCache.add(new GroupData().withId(id).withName(name));
     }
-    return new Groups(groupCash);
+    return new Groups(groupCache);
   }
 
 }
