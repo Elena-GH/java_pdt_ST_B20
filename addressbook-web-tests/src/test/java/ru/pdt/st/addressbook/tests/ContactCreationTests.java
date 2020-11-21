633bfd442a7e2eb8eb515cee0050cf164ec62af6
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-21 20:08:25 +0300

    ContactCreationTests. Рефакторинг условий - Hamcrest и ForwardigSet (fluent)

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/ContactCreationTests.java
@@ -1,17 +1,18 @@
 package ru.pdt.st.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.pdt.st.addressbook.model.ContactData;
+import ru.pdt.st.addressbook.model.Contacts;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
     app.goTo().homePage();
-    Set<ContactData> befor = app.contact().all();
+    Contacts befor = app.contact().all();
     ContactData contact = new ContactData()
             .withFirstName("Contact_First_Name")
             .withLastName("Contact_Last_Name")
@@ -19,16 +20,16 @@ public class ContactCreationTests extends TestBase {
             .withEmail("contact_mail@gmail.com")
             .withGroup("Group_Name");
     app.contact().create(contact);
-    Set<ContactData> after = app.contact().all();
-    Assert.assertEquals(after.size(), befor.size() + 1);
+    Contacts after = app.contact().all();
+    assertThat(after.size(), equalTo(befor.size() + 1));
 
-    // Постулируется, что дабавленный элемент имеет максимальный идентификатор
-    // Поиск объекта с максимальным идентификатором
-    contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt());
-    befor.add(contact);
     // Сравнение списков групп до и после теста с помощью множеств (неупорядоченные коллекции)
-    // При этом сравнение выполняется средствами тестовго фреймворка testng
-    Assert.assertEquals(befor, after);
+    // Постулируется, что дабавленный элемент имеет максимальный идентификатор
+    // Для реализации fluent-интерфейса (вытягивания в цепочку) сравниваются копии множества after и befor
+    // Расширение методов для HashSet реализуется через интерфейс ForwardingSet библиотеки Guava +withAdded
+    // При этом сравнение выполняется средствами подключенной библиотеки Hamcrest +assertThat +equalTo
+    assertThat(after, equalTo(
+            befor.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/GroupCreationTests.java
@@ -19,10 +19,10 @@ public class GroupCreationTests extends TestBase {
     assertThat(after.size(), equalTo(befor.size() + 1));
 
     // Сравнение списков групп до и после теста с помощью множеств (неупорядоченные коллекции)
+    // Постулируется, что дабавленный элемент имеет максимальный идентификатор
     // Для реализации fluent-интерфейса (вытягивания в цепочку) сравниваются копии множества after и befor
     // Расширение методов для HashSet реализуется через интерфейс ForwardingSet библиотеки Guava +withAdded
     // При этом сравнение выполняется средствами подключенной библиотеки Hamcrest +assertThat +equalTo
-    // Постулируется, что дабавленный элемент имеет максимальный идентификатор
     assertThat(after, equalTo(
             befor.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
