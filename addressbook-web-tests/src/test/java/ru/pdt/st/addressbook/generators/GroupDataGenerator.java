192c37c30fa08abca0770f7822f256f98ca15547
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-11-28 15:25:01 +0300

    GroupCreationTests. Генерация и чтене тестовых данных в формате xml

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,7 @@ package ru.pdt.st.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.pdt.st.addressbook.model.GroupData;
 
 import java.io.File;
@@ -20,6 +21,9 @@ public class GroupDataGenerator {
   @Parameter(names = "-f", description = "Target file")
   public String file;
 
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
   public static void main(String[] args) throws IOException {
     GroupDataGenerator generator = new GroupDataGenerator();
     // new JCommander(generator, args); - Deprecated API usage
@@ -36,11 +40,26 @@ public class GroupDataGenerator {
 
   private void run() throws IOException {
     List<GroupData> groups = generateGroups(count);
-    save(groups, new File(file));
+    if (format.equals("csv")) {
+      saveAsCsv(groups, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(groups, new File(file));
+    } else {
+      System.out.println("Unrecognized format " + format);
+    }
   }
 
-  private void save(List<GroupData> groups, File file) throws IOException {
-    System.out.println(new File(".").getAbsolutePath());
+  private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    String xml = xstream.toXML(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+  private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
+    // System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
       writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
@@ -53,9 +72,9 @@ public class GroupDataGenerator {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
       groups.add(new GroupData()
-              .withName(String.format("Group_Name_%s", i + 1))
-              .withHeader(String.format("Group_Header_%s", i + 1))
-              .withFooter(String.format("Group_Footer_%s", i + 1)));
+              .withName(String.format("Group_Name %s", i + 1))
+              .withHeader(String.format("Group_Header \n%s", i + 1))
+              .withFooter(String.format("Group_Footer \n%s", i + 1)));
     }
     return groups;
   }
