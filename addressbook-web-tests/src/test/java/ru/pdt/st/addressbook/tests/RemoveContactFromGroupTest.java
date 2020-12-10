876a667521d1b82f78ec59d0be5aa83e85a00ddd
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-12-10 21:05:31 +0300

    RemoveContactFromGroupTest. Добавлено предусловие

--- addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/RemoveContactFromGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/pdt/st/addressbook/tests/RemoveContactFromGroupTest.java
@@ -1,5 +1,6 @@
 package ru.pdt.st.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.pdt.st.addressbook.model.ContactData;
 import ru.pdt.st.addressbook.model.Contacts;
@@ -10,6 +11,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RemoveContactFromGroupTest extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions() {
+    Contacts contacts = app.db().contacts();
+    Groups groups = app.db().groups();
+    List<GroupData> groupList = new ArrayList<>();
+    for (GroupData g : groups) {
+      if (g.getContacts().size() != 0) {
+        groupList.add(g);
+      }
+    }
+    if (groupList.size() == 0 || contacts.size() == 0) {
+      app.goTo().homePage();
+      app.contact().create(new ContactData()
+              .withFirstName("Contact_First_Name")
+              .withLastName("Contact_Last_Name")
+              .withAddress("123456, г.Москва, Московский пр-т, д.10, стр.1")
+              .withMobilePhone("+7 (123) 123-45-67")
+              .withWorkPhone("+7 (456) 123-45-67")
+              .withEmail("contact_mail@gmail.com")
+              .withEmail2("contact_mail_2@gmail.com")
+              .withGroup("Group_Name"));
+    }
+  }
 
   @Test
   public void testRemoveContactFromGroup() {
