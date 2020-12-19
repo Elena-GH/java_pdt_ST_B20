4b0e578d4a55da2238c1cb17d1b3b74b1fb30b56
Elena-GH <73426245+Elena-GH@users.noreply.github.com>
2020-12-19 22:14:59 +0300

    SOAP-Sample. Добавлен тестовый класс GeoIPServiceTests

---
+++ soap-sample/src/test/java/ru/pdt/st/soap/GeoIPServiceTests.java
@@ -0,0 +1,25 @@
+package ru.pdt.st.soap;
+
+import com.lavasoft.*;
+import com.lavasoft.GeoIPServiceHttpGet;
+import com.lavasoft.GeoIPServiceSoap;
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class GeoIPServiceTests {
+
+  @Test
+  public void testMyIPPositive() {
+    String geoIP = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("64.18.87.4");
+    System.out.println(geoIP);
+    assertTrue(geoIP.contains("CA"));
+  }
+
+  @Test
+  public void testMyIPNegative() {
+    String geoIP = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("64.18.87.4");
+    assertTrue(geoIP.contains("RU"));
+  }
+
+}
